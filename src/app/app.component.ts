f93dc4ba976c55ddd8981cadcb6f6dff56fcb91a
Fjeldsoe <fjeldsoe@gmail.com>
2019-01-13 08:52:54 +0100

    fixed minor bug when completing a todo + styling fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo-app';
+  title = 'My To-Do list';
   
   constructor() {}
   
